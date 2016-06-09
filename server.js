1f3954abe083852aa0e843e1335e6f3aa5dd5a3e
Atte Kortesmaa <atte.kortesmaa@gmail.com>
2016-06-09 15:06:10 +0300

    Basic server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser');
+    
+var app = express();
+app.use(express.static(__dirname + "/public"));
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set server port to listen
+app.set('port', (process.env.PORT || 3000));
+
+// Run the server
+app.listen(app.get('port'), function() {
+   console.log('Server running on: ' + app.get('port'));
+});
