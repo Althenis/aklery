a011fb33f1a4490eee602d0ff06cc7d733f5855c
Atte Kortesmaa <atte.kortesmaa@gmail.com>
2016-06-29 14:58:03 +0300

    Moment.js, format comment date with moment's Time from X, sort comments so most recent is on top

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,36 @@
 var gulp = require('gulp'),
-    gls = require('gulp-live-server');
+    nodemon = require('gulp-nodemon');
 
 gulp.task('default', function() {
-    var server = gls.new('server.js');
-    server.start();
-    
-    gulp.watch('public/**/*', function(file) {
-        server.notify.apply(server, [file]);
-        console.log('refreshing');
+    nodemon({
+        script: 'server.js',
+        ext: 'html js json',
+        env: { 'NODE_ENV': 'development'}
     });
-    
-    gulp.watch('server.js', function() {
-        server.start.bind(server);
-        console.log('Server restarted');
-    });
-    
 });
+
+// gulp.task('default', function() {
+//     var server = gls('server.js');
+//     server.start().then(function(result) {
+//         console.log('Server exited with result:', result);
+//         process.exit(result.code);
+//    });
+//    
+//    gulp.watch(['server.js','public/**/*'], function(file) {
+//         server.start.bind(server);
+//         server.notify.apply(server, [file]);
+//         
+//     });
+//     gulp.watch('server.js', server.start);
+//     
+//     gulp.watch('public/**/*', function(file) {
+//         server.notify.apply(server, [file]);
+//         console.log('refreshing');
+//     });
+//     
+//     gulp.watch('server.js', function() {
+//         server.start.bind(server);
+//         console.log('Server restarted');
+//     });
+//     server.stop();
+// });
--- public/controllers/main.js
+++ public/controllers/main.js
@@ -16,11 +16,17 @@ angular.module('aklery')
     vm.addComment = function() {
         console.log("id: " + vm.postContent._id + " user: " + vm.comment.username + " comment: " + vm.comment.comment);
         vm.comment.id = vm.postContent._id;
+        vm.postContent.comments.push(vm.comment);
         $http.post('/comments/add',vm.comment).then(function() {
             console.log('ok');
         }, function() {
             console.log('err');
         });
+    };    
+}]).filter('commentDate', function($filter) {
+    return function(input) {
+        var postDate = moment(input);
+        var currentDate = moment();
+        return postDate.from(currentDate);
     };
-    
-}]);
+});
